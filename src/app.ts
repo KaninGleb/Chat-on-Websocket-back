dd30deacda43b875e2e83be6995c867c0b6c8b2f
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-12 19:33:47 +0300

    refactor: delete unnecessary comments

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
-// import cors from 'cors'
 
 const app = express()
-// app.use(cors())
 
 const httpServer = createServer(app)
-// const socket = socketio(httpServer)
 
 const messages: any[] = [
   { message: 'Hello, Viktor', id: 'hsdafgds', user: { id: 'sdfdsf', name: 'Dimych' } },
